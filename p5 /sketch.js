12db1b283ad0bc897d6fc0f03d3ef07b301d7783
akarklina <anitakarklina@yahoo.co.uk>
2024-12-10 14:59:29 +0000

    create circle

--- p5 /sketch.js
+++ p5 /sketch.js
@@ -1,7 +1,13 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 400); // Create a 400x400 pixel canvas
 }
 
 function draw() {
-  background(220);
+  background(220); // Set background color to light gray
+
+  fill(0, 102, 255); // Set fill color for the circle (blue)
+  stroke(0); // Set the outline color (black)
+  strokeWeight(2); // Set the thickness of the outline
+
+  ellipse(200, 200, 100, 100); // Draw a circle at (200, 200) with a diameter of 100
 }
